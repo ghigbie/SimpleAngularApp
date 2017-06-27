b1ba046c40d27a911717b494164eff8778b7e38c
George Higbie <gehigbie@gmail.com>
2017-06-27 16:34:39 +0000

    Add css file with style

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express'),
 app.set('port', process.env.PORT);
 app.set('ip', process.env.IP);
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
 app.listen(app.get('port'), app.get('ip'), () => {
     const port = app.get('port');
     console.log(`The server is listening on port ${port}.`);
